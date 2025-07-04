810521c07c00fe577f147460a01751c7e83f0363
shubhamieshubhism <shubhamtirpude20@gmail.com>
2025-07-04 15:35:19 +0530

    Palindrome commit

--- src/test/java/Java/palindrome.java
+++ src/test/java/Java/palindrome.java
@@ -1,4 +1,32 @@
 package Java;
 
 public class palindrome {
+    public static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void findPalindrome(String str){
+        for(int i =0 ; i<str.length();i++){
+            for(int j=i+1;j<str.length();j++){
+                String subString = str.substring(i,j);
+                if(isPalindrome(subString)){
+                    System.out.println(subString);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String input = "madam";
+        findPalindrome(input);
+    }
 }
